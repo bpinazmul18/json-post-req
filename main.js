f33491b1954a0830d7e5b27331d73988e107f7ca
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-01-17 12:24:40 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// var x = fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// var y = x.map( value=> {
+//     return value;
+// })
+//
+// console.log(y)

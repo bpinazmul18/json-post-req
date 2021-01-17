c19a470587e944fc6741c43da539653455e3b8c5
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-01-17 16:55:38 +0600

    forEach function

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-(function($) {
-    "use strict";
-
+function get() {
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'GET',
         headers: {
@@ -11,11 +9,28 @@
         .then((response) => response.json())
         .then((obj) =>{
             for(var i in obj) {
-                $('#post').click(function(){
-                    $("#result").append(`<p>${obj[i].id}</p>`);
-                })
+                $("#result").append(`<p>${obj[i].body}</p>`);
             }
-        });
+        })
+}
 
 
-})(jQuery);
+
+function email() {
+    fetch('https://jsonplaceholder.typicode.com/comments', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+
+        .then((response) => response.json())
+        .then((obj) =>{
+            // for(var i in obj) {
+            //     $("#result").append(`<p>${obj[i].email}</p>`);
+            // }
+            obj.forEach(value => {
+                    $("#result").append(`<p>${value.email}</p>`);
+            })
+        })
+}
